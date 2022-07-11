78f391f7c595134d31605700c1b5db9e0876e442
jdouglas <jefferson.douglas@foursys.com.br>
2022-07-11 13:13:54 -0300

    commit inicial

---
+++ app/src/main/java/com/example/projetosorteio/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.projetosorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void sortearNumero(View view){
+        TextView texto = findViewById(R.id.textoResultado);
+
+        int  numero = new Random().nextInt(11);
+
+
+        texto.setText("numero: " + numero);
+    }
+
+}
